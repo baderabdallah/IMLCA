63faeaa1a12d3917429c8908cac1372cdc4287e9
Simone Pongiglione <simone.pongiglione@dxc.com>
2024-07-02 15:50:14 +0200

    Add computation of avg speed KPI

--- src/kpis_tracker/src/kpis_tracker_node.py
+++ src/kpis_tracker/src/kpis_tracker_node.py
@@ -14,11 +14,21 @@ class KPIsTrackerNode():
         self.mlc_subscriber = rospy.Subscriber(
             mlc_topic, Mlc, self.mlc_callback)
 
+        self.overall_sum_of_velocity_samples = 0
+        self.overall_count_of_velocity_samples = 0
+        self.overall_avg_of_velocity_samples = 0
+
     def mlc_callback(self, msg):
         self.latest_msg = msg
+        self.update_overall_avg_velocity(msg)
 
     def spin(self):
-        rospy.spin()        
+        rospy.spin()
+
+    def update_overall_avg_velocity(self, msg):
+        self.overall_count_of_velocity_samples += 1
+        self.overall_sum_of_velocity_samples += msg.ego_info.velocity_x
+        self.overall_avg_of_velocity_samples = self.overall_sum_of_velocity_samples / self.overall_count_of_velocity_samples
 
 # Main function.
 if __name__ == '__main__':
@@ -27,6 +37,8 @@ if __name__ == '__main__':
     try:
         kpis_tracker_node = KPIsTrackerNode()
         kpis_tracker_node.spin()
+
+        raise RuntimeError()
 
     except rospy.ROSInterruptException as err:
         print(f"[KPIs tracker] An error occured: {err}")
